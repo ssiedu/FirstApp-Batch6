a35b127a460820945fc984d02f0cd00b2169bedf
manoj <manoj.sarwate@ssiedu.in>
2021-04-06 19:04:03 +0530

    060421

---
+++ src/LabelExampleOne.java
@@ -0,0 +1,21 @@
+
+public class LabelExampleOne {
+
+	public static void main(String[] args) {
+
+		p: for(int i=1; i<=5;i++) {
+			
+			q: for(int j=1; j<=5; j++) {
+				
+				if(j==3) break p;
+				
+				System.out.println(j);
+			}
+			
+		}
+		
+		
+
+	}
+
+}
---
+++ src/LabelExampleTwo.java
@@ -0,0 +1,21 @@
+
+public class LabelExampleTwo {
+
+	public static void main(String[] args) {
+		p: 
+		for (int i = 1; i <= 5; i++) {
+			
+			 q:
+			 for (int j = 1; j <= 5; j++) {
+
+				if (j == 3)
+					continue p;
+
+				System.out.println(j);
+			}
+
+		}
+
+	}
+
+}
